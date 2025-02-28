9cc412f3cfc76b16292a498c22e8361af473006b
Ted Benson <edward.benson@gmail.com>
2025-02-28 11:49:47 -0500

    Update zcolors.plugin.zsh

--- zcolors.plugin.zsh
+++ zcolors.plugin.zsh
@@ -27,7 +27,8 @@ zcolors.plugin() {
     # These lines are attempted fixes but also caused problems
     # less+=( --use-color '-DSkY' '-DPWb' )
     # less+=( --use-color '-Ds30;103p97;44' )
-    less=( --use-color '-Ds30;103p97;44' )
+    # less=( --use-color '-Ds30;103p97;44' )
+    less+=( --use-color '-DSkY -DPWb'  )
   fi
 
   add-zsh-hook precmd .zcolors.precmd
